55087ff2a1e321990ec7ad4e7aededf768931665
Yusuf Uyanoğlu <uyanogluyusuf@gmail.com>
2025-12-07 19:49:15 +0300

    Setup Sanic, Docker, MySQL and CI/CD

---
+++ database.py
@@ -0,0 +1,41 @@
+import aiomysql
+import os
+
+pool = None
+
+async def init_db(app, loop):
+    global pool
+    pool = await aiomysql.create_pool(
+        host=os.getenv('DB_HOST'),
+        port=3306,
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        db=os.getenv('MYSQL_DATABASE'),
+        loop=loop,
+        autocommit=True
+    )
+    print("\n ---Veritabani baglantisi basarili!--- \n")
+
+async def close_db(app, loop):
+    global pool
+    if pool:
+        pool.close()
+        await pool.wait_closed()
+
+async def execute_query(query, args=()):
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(query, args)
+            return cur.lastrowid
+
+async def fetch_all(query, args=()):
+    async with pool.acquire() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(query, args)
+            return await cur.fetchall()
+
+async def fetch_one(query, args=()):
+    async with pool.acquire() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(query, args)
+            return await cur.fetchone()
---
+++ main.py
@@ -0,0 +1,22 @@
+from sanic import Sanic
+from sanic.response import json
+from dotenv import load_dotenv
+from database import init_db, close_db, fetch_all
+
+load_dotenv()
+
+app = Sanic("CampusHub")
+
+app.register_listener(init_db, "before_server_start")
+app.register_listener(close_db, "after_server_stop")
+
+@app.route("/")
+async def test(request):
+    users = await fetch_all("SELECT * FROM Users")
+    return json({
+        "message": "CampusHub Backend Çalışıyor!", 
+        "users": users
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
